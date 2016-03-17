d67de7ca85a740ac39da9209d6a4419c0b46af8c
Kafil NASDAMI <kafil.nasdami@gmail.com>
2016-03-17 16:28:24 +0000

    maximalAllowableSubbarrays.js corrected

---
+++ Javascript/tasks/missingNumber.js
@@ -0,0 +1,9 @@
+function missingNumber(arr) {
+	var n = arr.length + 1,
+		sum = 0,
+		expectedSum = n * (n + 1) / 2;
+	for (var i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return expectedSum - sum;
+}
