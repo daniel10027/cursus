e52f3db7ac1cb44d37476b1f600fd412dfbfa0bc
Kafil <kafil.nasdami@gmail.com>
2016-05-17 16:51:36 +0000

    Videopart solved

---
+++ Javascript/tasks/videoPart.js
@@ -0,0 +1,18 @@
+function videoPart(part, total) {
+
+  var getSeconds = function(time) {
+    var h = parseInt(time.substring(0, 2)),
+        m = parseInt(time.substring(3, 5)),
+        s = parseInt(time.substring(6, 8));
+    return h * 60 * 60 + m * 60 + s;
+  }
+
+  var gcd = function(a, b) {
+      return !b?a:gcd(b,a%b);
+  }
+
+  var partTime = getSeconds(part);
+  var totalTime = getSeconds(total);
+  var divisor = gcd(partTime, totalTime);
+  return [partTime / divisor, totalTime / divisor];
+}
