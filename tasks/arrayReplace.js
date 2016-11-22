8dfeab5f5d840ccbd8b76a8dbd29736b4b9b7372
Kafil Nasdami <kafil.nasdami@gmail.com>
2016-11-22 21:31:33 +0000

    arrayReplace.js corrected

--- tasks/arrayReplace.js
+++ tasks/arrayReplace.js
@@ -1,7 +1,7 @@
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
   for (var i = 0; i < inputArray.length; i++) {
     if (inputArray[i] === elemToReplace) {
-      substitutionElem = inputArray[i];
+      inputArray[i] = substitutionElem;
     }
   }
   return inputArray;
