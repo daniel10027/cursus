8a5a3742e5b3eb5e04750cb32b4030455a50e543
Kafil Nasdami <kafil.nasdami@gmail.com>
2016-11-27 01:11:48 +0000

    eratosthenessieve improved

--- tasks/eratosthenesSieve.js
+++ tasks/eratosthenesSieve.js
@@ -1,13 +1,36 @@
+//bugfix
 function eratosthenesSieve(n) {
-    function isP(n) {
-        for (var i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
+
+  var primes = [],
+    mayBePrime = [];
+  for (var i = 0; i <= n; i++) {
+    mayBePrime.push(true);
+  }
+  for (var i = 2; i <= n; i++) {
+    if (mayBePrime[i]) {
+      primes.push(i);
+      for (var j = 1; i * j <= n; j++) {
+        mayBePrime[i * j] = false;
+      }
     }
-    var ans = [];
-    for (var i = 2; i <= n; i++) {
-        if (isP(i)) ans.push(i)
-    }
-    return ans;
+  }
+
+  return primes;
+}
+
+//Codewriting
+function eratosthenesSieve(n) {
+  function isP(n) {
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0)
+        return false;
+      }
+    return true;
+  }
+  var ans = [];
+  for (var i = 2; i <= n; i++) {
+    if (isP(i))
+      ans.push(i)
+  }
+  return ans;
 }
